447de1d047373b73063053f7bde29fb82802164f
Bowie Xu <fantast.xu@gmail.com>
2014-08-30 13:44:02 +0800

    add plist read class

---
+++ Assets/Scipts/PlistTest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using PlistCS;
+
+public class PlistTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		TextAsset plistasset = Resources.Load<TextAsset>("ChainReaction");
+
+		Dictionary<string, object> dic2 = (Dictionary<string, object>)Plist.readPlist(plistasset.bytes);
+		Dictionary<string, object> girldic = (Dictionary<string, object>)dic2["Girl"];
+		Dictionary<string, object> santance1 = (Dictionary<string, object>)girldic["1"];
+		foreach (string key in santance1.Keys) {
+			Debug.Log(key+" is "+santance1[key].GetType());		
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
